54dc0461e901dd5bcc9e00018451ef1308b7bcfa
Michael Chen <michaelchengaming@gmail.com>
2020-08-17 14:30:25 -0700

    add dummy daata from json file and test creating -- works

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const bootcamps = require("./routes/bootcamps");
 const app = express();
 
 //##### Middleware starts |||| app.use() <- is middleware #########
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
