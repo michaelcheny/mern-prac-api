7faf0103daec5d674bdb537c559698db4f22a6de
Michael Chen <michaelchengaming@gmail.com>
2020-08-17 12:35:20 -0700

    test custom middleware, add morgan for dev logging middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // Route Files
 const bootcamps = require("./routes/bootcamps");
@@ -9,10 +11,20 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// Middleware
+// Middleware starts |||| app.use() <- is middleware
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Middleware ends
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
