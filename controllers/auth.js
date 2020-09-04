bfa23d0160e68a102424fe2e97c14c13e8051f64
Michael Chen <michaelchengaming@gmail.com>
2020-09-03 23:39:26 -0700

    add bcrypt to salt and hash pw, create user works

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,16 @@ const User = require('../models/User');
 // @route   Get /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
+  // res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({ success: true });
 });
