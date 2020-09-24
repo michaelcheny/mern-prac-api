9e730861bd472192894ddb1f25a8f9b022fa05ea
Michael Chen <michaelchengaming@gmail.com>
2020-09-23 23:38:43 -0700

    add middleware to authorize user roles for bc and cources

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,7 +18,7 @@ const courseRouter = require('./courses');
 const router = express.Router();
 
 // Middleware for authorizing
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Reroute into other resource routers
 // whenever this is hit, it uses courseRouter
@@ -26,17 +26,19 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
